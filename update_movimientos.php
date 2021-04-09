4dc060f15797cc94acd56d1e7f19d5f70079cb06
= <leochabur@gmail.com>
2021-04-09 19:37:22 -0300

    ok

--- update_movimientos.php
+++ update_movimientos.php
@@ -1,14 +1,14 @@
 <?php
 
 
-	$pdo = $enlace = mysqli_connect("127.0.0.1", "root", "leo181979", "gestionfaena");
+	$pdo = $enlace = mysqli_connect("127.0.0.1", "root", "leo1979", "gestionfaena");
     
 
     $result = mysqli_query($pdo,   'SELECT s.id
 FROM sp_st_mov_abst s
 join sp_proc_fan_day pfd on pfd.id = s.id_proc_fan_day
 join sp_proc_fan pf on pf.id = id_proc_fan
-where pf.id = 13');
+where pf.id = 14');
     while ($row = mysqli_fetch_array($result))
     {
     	$id = $row['id'];
